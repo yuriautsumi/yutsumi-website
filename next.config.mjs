7f5dd49195750716b9622f732f01b5ec0f848b94
ibelick <julien.thibeaut@gmail.com>
2024-12-19 15:02:40 +0100

    feat: init template

--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,10 @@
-import mdx from "@next/mdx";
-
-const withMDX = mdx({
-  extension: /\.mdx?$/,
-  options: {},
-});
+import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
-  transpilePackages: ["next-mdx-remote"],
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.google.com",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "news.mit.edu",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.eecs.mit.edu",
-        pathname: "**",
-      },
-    ],
-  },
-  sassOptions: {
-    compiler: "modern",
-    silenceDeprecations: ["legacy-js-api"],
-  },
-  output: "standalone",
-  allowedDevOrigins: [
-    "http://localhost:3000",
-    "https://z3yjxs3dya.us-east-2.awsapprunner.com",
-    "https://yuriautsumi.com",
-  ],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
+
+const withMDX = createMDX();
 
 export default withMDX(nextConfig);
