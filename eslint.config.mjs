9abe057bfd70cd1e8d1155db3786cf26e635f501
Yuria U <yuriautsumi@users.noreply.github.com>
2026-01-24 06:49:01 +0900

    add lint modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,9 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
 import { FlatCompat } from '@eslint/eslintrc'
-
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals.js'
+ 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
@@ -67,4 +69,13 @@ export default [
       '@typescript-eslint/ban-ts-comment': 'off',
     },
   },
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
 ]
