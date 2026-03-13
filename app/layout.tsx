adae5b195cc7ec78eb64feef45224609130a3f5b
Yuria U <yuriautsumi@users.noreply.github.com>
2026-03-13 00:05:43 -0500

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ export const metadata: Metadata = {
     canonical: '/'
   },
   title: {
-    default: 'Nim - Personal website template',
-    template: '%s | Nim'
+    default: 'Yuria Utsumi | Portfolio',
+    template: '%s | Portfolio'
   },
-  description:  'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
+  description:  'Yuria Utsumi is a Machine Learning Engineer.',
 };
 
 const geist = Geist({
